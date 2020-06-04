16e509eed9f4dd674006be03a9f5d7bfdd4a91ff
kudoshunsaku <kudo.shunsaku@gmail.com>
2020-06-04 17:54:05 +0900

    use MySQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,13 +74,37 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+"""
+# postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'kudoshunsaku',
+        'PASSWORD': 'tantei',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangogirls',
+        'USER': 'kudoshunsaku',
+        'PASSWORD': 'tanteitantei',
+        'HOST': 'localhost',
+        'PORT': '3306',
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
